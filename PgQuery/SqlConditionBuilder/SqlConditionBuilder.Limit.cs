092b49acdca29575ebb189890a0122042530a5a0
ponlawat-w <ponlawat_w@cmu.ac.th>
2018-09-13 14:25:36 +0700

    Remove Unsed Includes

--- PgQuery/SqlConditionBuilder/SqlConditionBuilder.Limit.cs
+++ PgQuery/SqlConditionBuilder/SqlConditionBuilder.Limit.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PgQuery
+﻿namespace PgQuery
 {
     public abstract partial class SqlConditionBuilder : SqlBuilder
     {
